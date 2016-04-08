4e0a7ddb9040a5ae42191c63cc30e0451a6215f5
Bao Tran <btran.it@gmail.com>
2016-04-08 07:15:10 -0700

    add seek slider
    add seek slider and even listeners

--- js/vid.js
+++ js/vid.js
@@ -1,10 +1,33 @@
-function playPause(btn,vid) {
-              var vid = document.getElementById(vid);
-              if(vid.paused) {
-                  vid.play();
-                  btn.innerHTML = "Pause";
-              } else {
-                  vid.pause();
-                  btn.innerHTML = "Play";
-              }
+var vid, playbtn, seekslider;
+
+function initializePlayer() {
+  // Set object references
+  vid = document.getElementById("my_video");
+  playbtn = document.getElementById("playpausebtn");
+  seekslider = document.getElementById("seekslider");
+  // Add event listeners
+  playbtn.addEventListener("click",playPause,false);
+  seekslider.addEventListener("change",vidSeek,false);
+  vid.addEventListener("timeupdate",seekTimeupdate,false);
+}
+window.onload = initializePlayer;
+
+function playPause() {
+    if(vid.paused) {
+      vid.play();
+      playbtn.innerHTML = "Pause";
+    } else {
+      vid.pause();
+      playbtn.innerHTML = "Play";
+    }
 } 
+
+function vidSeek() {
+    var seekto = vid.duration * (seekslider.value / 100);
+    vid.currentTime = seekto;
+}
+
+function seekTimeupdate() {
+    var newtime = vid.currentTime * (100 / vid.duration);
+    seekslider.value = newtime;
+}
