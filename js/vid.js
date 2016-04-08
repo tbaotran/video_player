7ffdd59f73a42e6b6d6039a071b7a8500dfb0b9e
Bao Tran <btran.it@gmail.com>
2016-04-08 13:06:58 -0700

    Add time display

--- js/vid.js
+++ js/vid.js
@@ -1,10 +1,12 @@
-var vid, playbtn, seekslider;
+var vid, playbtn, seekslider, curtimetext, durtimetext;
 
 function initializePlayer() {
   // Set object references
   vid = document.getElementById("my_video");
   playbtn = document.getElementById("playpausebtn");
   seekslider = document.getElementById("seekslider");
+  curtimetext = document.getElementById("curtimetext");
+  durtimetext = document.getElementById("durtimetext");
   // Add event listeners
   playbtn.addEventListener("click",playPause,false);
   seekslider.addEventListener("change",vidSeek,false);
@@ -30,4 +32,16 @@ function vidSeek() {
 function seekTimeupdate() {
     var newtime = vid.currentTime * (100 / vid.duration);
     seekslider.value = newtime;
+
+    var curmins = Math.floor(vid.currentTime / 60);
+    var cursecs = Math.floor(vid.currentTime - curmins * 60);
+    var durmins = Math.floor(vid.duration / 60);
+    var dursecs = Math.floor(vid.duration - durmins * 60);
+    if(cursecs < 10) { cursecs = "0"+cursecs; }
+    if(dursecs < 10) { dursecs = "0"+dursecs; }
+    if(curmins < 10) { curmins = "0"+curmins; }
+    if(durmins < 10) { durmins = "0"+durmins; }
+
+    curtimetext.innerHTML = curmins+":"+cursecs;
+    durtimetext.innerHTML = durmins+":"+dursecs;
 }
